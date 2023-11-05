ea45ab7ab7895ebedee98075334a2f13db2323d9
Ксения Смолякова <smalyokova.ksenya.05@mail.ru>
2023-11-05 22:00:07 +0500

    Функции возвращаются

--- js/functions.js
+++ js/functions.js
@@ -18,3 +18,25 @@ console.log(isPalindrom('проверяемая строка')); // false
 console.log(isPalindrom('Алла')); // true
 console.log(isPalindrom('ДовОд')); // true
 console.log(isPalindrom('Лёша на полке клопа нашёл ')); // true
+
+
+//Функция, которая принимает время начала и конца рабочего дня, а также время старта и продолжительность встречи в минутах и возвращает true, если встреча не выходит за рамки рабочего дня, и false, если выходит
+const convertToMinutes = (arr) => ((+arr[0] * 60) + +arr[1]);
+
+const checkTime = (start, end, startMeeting, duration) => {
+  const startTime = start.split(':');
+  const endTime = end.split(':');
+  const startMeetingTime = startMeeting.split(':');
+
+  const startMinutesTime = convertToMinutes(startTime);
+  const endMinutesTime = convertToMinutes(endTime);
+  const startMeetingMinutesTime  = convertToMinutes(startMeetingTime);
+
+  return startMeetingMinutesTime >= startMinutesTime && startMeetingMinutesTime + duration <= endMinutesTime;
+};
+
+checkTime('08:00', '17:30', '14:00', 90);
+checkTime('8:0', '10:0', '8:0', 120);
+checkTime('08:00', '14:30', '14:00', 90);
+checkTime('14:00', '17:30', '08:0', 90);
+checkTime('8:00', '17:30', '08:00', 900);
