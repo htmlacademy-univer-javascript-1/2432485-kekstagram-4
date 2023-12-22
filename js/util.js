ee3233f1fba4d13f29445cd24c4836c59e0eb211
Ксения Смолякова <smalyokova.ksenya.05@mail.ru>
2023-12-22 15:20:58 +0500

    Надо подкачаться

--- js/util.js
+++ js/util.js
@@ -1,3 +1,4 @@
+const ALERT_SHOW_TIME = 4000;
 function getRandomInteger(intFrom, intTo){
   if (intFrom > intTo || intFrom < 0 || intTo < 0 ){
     return  new Error('Error. Change input values');
@@ -17,5 +18,26 @@ function getUniqNumber (usersId) {
 
 const pressEscape = (evt) => evt.key === 'Escape';
 
-export {getUniqNumber, getRandomInteger, pressEscape, generateArray};
+const showAlert = (message) => {
+  const alertContainer = document.createElement('div');
+  alertContainer.style.zIndex = 100;
+  alertContainer.style.position = 'absolute';
+  alertContainer.style.left = 0;
+  alertContainer.style.top = 0;
+  alertContainer.style.right = 0;
+  alertContainer.style.padding = '10px 3px';
+  alertContainer.style.fontSize = '15px';
+  alertContainer.style.textAlign = 'center';
+  alertContainer.style.backgroundColor = 'red';
+
+  alertContainer.textContent = message;
+
+  document.body.append(alertContainer);
+
+  setTimeout(() => {
+    alertContainer.remove();
+  }, ALERT_SHOW_TIME);
+};
+
+export {getUniqNumber, getRandomInteger, pressEscape, generateArray, showAlert};
 
